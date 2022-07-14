923d8063b0b2a3a9050ddac345402656a8123a27
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-07-13 22:25:08 -0300

    creating the project base

---
+++ City Report/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  City Report
+//
+//  Created by Rayana Prata Neves on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
