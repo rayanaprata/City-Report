da03a39be4455b0d66dd3bf5dc92a0cea6e2e43d
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-07-19 00:04:10 -0300

    create CadastrarProblemaViewController properties

--- City Report/CadastrarProblemaViewController.swift
+++ City Report/CadastrarProblemaViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class CadastrarProblemaViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var nome: UITextField!
+    @IBOutlet weak var localizacao: UITextField!
+    @IBOutlet weak var descricao: UITextField!
+    @IBOutlet weak var imagem: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
+    private func setupUI() {
+        backgroundView.layer.cornerRadius = 10
+    }
+    
+    @IBAction func carregarImagem(_ sender: Any) {
+        
+    }
+    
+    @IBAction func salvar(_ sender: Any) {
+        
+        guard let nomeSafe = nome.text, let localizacaoSafe = localizacao.text, let descricaoSafe = descricao.text else { return }
+        
+        if !nomeSafe.isEmpty && !localizacaoSafe.isEmpty && !descricaoSafe.isEmpty {
+            
+            let context = DataBaseController.persistentContainer.viewContext
+            let problema = Problema(context: context)
+            problema.nome = nomeSafe
+            problema.descricao = descricaoSafe
+            problema.localizacao = localizacaoSafe
+            DataBaseController.saveContext()
+            
+            self.navigationController?.popToRootViewController(animated: true)
+            
+        } else {
+            
+            print(nomeSafe, localizacaoSafe, descricaoSafe)
+            
+            // tratamento de erro
+            // apresentar um alert informando pro usuário que todos os campos de texto devem ser preenchidos
+        }
+    }
 }
--- City Report/ProblemaUITableViewController.swift
+++ City Report/ProblemaUITableViewController.swift
@@ -36,7 +36,6 @@ class ProblemaUITableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return problemas.count
     }
